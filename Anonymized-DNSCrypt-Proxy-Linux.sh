7cfabe7c0f198f26177df920a8ebb37946013419
BL4CKH47H4CK3R <54761717+BL4CKH47H4CK3R@users.noreply.github.com>
2020-09-11 02:06:11 +0000

    Add files via upload

---
+++ Anonymized-DNSCrypt-Proxy-Linux.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo
+echo -e '+=============================================================+'
+echo -e '+               Anonymized-DNSCrypt-Proxy-Linux               +'
+echo -e '+      Automatically Configure Anonymized-DNSCrypt-Proxy      +'
+echo -e '+                   Coder : BL4CKH47H4CK3R                    +'
+echo -e '+=============================================================+'
+echo
+
+# Enable DNSCrypt-Proxy [Startup]
+systemctl enable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
+
+# Applying DNSCrypt-Proxy configurations
+rm -rf /etc/NetworkManager/NetworkManager.conf
+rm -rf /etc/resolv.conf
+rm -rf /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+echo -e '[device]\nwifi.scan-rand-mac-address=yes\n\n[main]\ndns=none' >/etc/NetworkManager/NetworkManager.conf
+echo -e 'nameserver 127.0.0.1\noptions edns0' > /etc/resolv.conf
+cp dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
+echo -e 'Set DNS Resolver ---> 127.0.0.1 [Permanently] [There Is No Place Like 127.0.0.1]\n'
+
+# Configure DNSCrypt-Proxy
+systemctl stop systemd-resolved -f
+systemctl disable systemd-resolved -f
+systemctl start dnscrypt-proxy.socket dnscrypt-proxy.service -f
+systemctl restart NetworkManager -f
+dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml -service restart
+dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml -check
+
+# Apply iptables ruleset
+iptables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+iptables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
+ip6tables -A OUTPUT -p tcp -j DROP
+ip6tables -A OUTPUT -p udp -j DROP
+
+# Termination Message
+echo -e '\nAnonymized DNSCrypt-Proxy Successfully Configured !\nEnjoy Anonymity & Show Middle Fingers ---> Snoopers/Spoofers !\n'
---
+++ README.md
@@ -0,0 +1,7 @@
+# Anonymized-DNSCrypt-Proxy-Linux
+Automatically Configure Anonymized-DNSCrypt-Proxy
+
+# Procedure (OneLiner)
+```
+git clone https://github.com/BL4CKH47H4CK3R/Anonymized-DNSCrypt-Proxy-Linux.git && cd Anonymized-DNSCrypt-Proxy-Linux && chmod +x Anonymized-DNSCrypt-Proxy-Linux.sh && sudo ./Anonymized-DNSCrypt-Proxy-Linux.sh
+```
