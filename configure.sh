b7f74ccc092c10e56666fb69ee7ac8a65ac03f04
BL4CKH47H4CK3R <54761717+BL4CKH47H4CK3R@users.noreply.github.com>
2020-09-12 01:30:08 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,7 +8,7 @@ echo -e '+                   Coder : BL4CKH47H4CK3R                    +'
 echo -e '+=============================================================+'
 echo
 
-# Enable DNSCrypt-Proxy [Startup]
+# Enabling DNSCrypt-Proxy [Startup]
 systemctl enable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
 
 # Applying DNSCrypt-Proxy configurations
@@ -20,21 +20,21 @@ echo -e 'nameserver 127.0.0.1\noptions edns0' > /etc/resolv.conf
 cp dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
 echo -e 'Set DNS Resolver ---> 127.0.0.1 [Permanently] [There Is No Place Like 127.0.0.1]\n'
 
-# Configure DNSCrypt-Proxy
+# Configuring DNSCrypt-Proxy
 systemctl stop systemd-resolved -f
-systemctl disable systemd-resolved -f
+systemctl disable --now systemd-resolved -f
 systemctl start dnscrypt-proxy.socket dnscrypt-proxy.service -f
 dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml -service restart
 dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml -check
 
-# Restart NetworkManager
-systemctl restart NetworkManager -f
-
-# Apply iptables ruleset
+# Applying IPTables Ruleset
 iptables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
 iptables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
 ip6tables -A OUTPUT -p tcp -j DROP
 ip6tables -A OUTPUT -p udp -j DROP
 
+# Restarting NetworkManager
+systemctl restart NetworkManager -f
+
 # Termination Message
-echo -e '\nAnonymized DNSCrypt-Proxy Successfully Configured !\nEnjoy Anonymity & Show Middle Fingers ---> Snoopers/Spoofers !\n'
+echo -e '\nAnonymized DNSCrypt-Proxy Successfully Configured !\nEnjoy Anonymity & Show Yout Middle Fingers ---> Snoopers !\n'
