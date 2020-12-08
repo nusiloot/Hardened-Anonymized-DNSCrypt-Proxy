6ad4b545b99f1bfa13b481ee0917002999a850f6
BL4CKH47H4CK3R <54761717+BL4CKH47H4CK3R@users.noreply.github.com>
2020-12-09 03:13:50 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-clear
-echo -e "\n####################################################################"
-echo -e "#                Hardened-Anonymized-DNSCrypt-Proxy                #"
-echo -e "#                Wipe Snoopers Out Of Your Networks                #"
-echo -e "#                       Coder : BL4CKH47H4CK3R                     #"
-echo -e "####################################################################\n"
-
 if [[ ${UID} != 0 ]]
 then
-	echo -e "Run The Script As Root, Are Your Drunk Dude ?"
+	echo -e "============================"
+	echo -e "|  Run The Script As Root  |"
+	echo -e "============================"
 else
-	echo -e "[1] Configure"
-	echo -e "[2] Deconfigure"
+	clear
+	echo -e "============================================"
+	echo -e "|    Hardened-Anonymized-DNSCrypt-Proxy    |"
+	echo -e "|    Wipe Snoopers Out Of Your Networks    |"
+	echo -e "|          Coder : BL4CKH47H4CK3R          |"
+	echo -e "============================================"
+	echo -e "|   [1] Configure     |"
+	echo -e "|   [2] Deconfigure   |"
+	echo -e "======================="
 	read -p "[*] Enter Choice [1 or 2]: " input
 
 	if [[ ${input} == 1 ]]
-	then
-		echo -e "\n[*] Installing DNSCrypt-Proxy ...\n"
+	then	
+		echo -e "==================================="
+		echo -e "|  Installing DNSCrypt-Proxy ...  |"
+		echo -e "==================================="
 		if ! [ -z `which pacman 2> /dev/null` ] && [ `nmcli networking` = "enabled" ]; # Arch
 		then
-			pacman -Syy dnscrypt-proxy --needed --noconfirm
+			pacman -S dnscrypt-proxy --needed --noconfirm
 		elif ! [ -z `which apt 2> /dev/null` ] && [ `nmcli networking` = "enabled" ]; # Debian
 		then
-			echo "deb https://deb.debian.org/debian/ testing main" | sudo tee /etc/apt/sources.list.d/testing.list && apt update && apt install -y -t testing dnscrypt-proxy
+			echo "deb https://deb.debian.org/debian/ testing main" | sudo tee /etc/apt/sources.list.d/testing.list
+			apt update && apt install -y -t testing dnscrypt-proxy
 		elif ! [ -z `which emerge 2> /dev/null` ] && [ `nmcli networking` = "enabled" ]; # Gentoo
 		then
 			emerge dnscrypt-proxy -av
 		elif ! [ -z `which apk 2> /dev/null` ] && [ `nmcli networking` = "enabled" ]; # Alpine
 		then
-			sh -c 'echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories' && apk add --upgrade dnscrypt-proxy
+			sh -c 'echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories'
+			apk add --upgrade dnscrypt-proxy
 		elif ! [ -z `which dnf 2> /dev/null` ] && [ `nmcli networking` = "enabled" ]; # Fedora
 		then
 			dnf install dnscrypt-proxy
@@ -46,47 +52,70 @@ else
 		then
 			eopkg install dnscrypt-proxy
 		fi
-		
-		echo -e "\n[*] Disabling SystemD-Resolved ..."
+
+		echo -e "======================================"
+		echo -e "|    Disabling SystemD-Resolved ...  |"
+		echo -e "======================================"
 		systemctl disable --now systemd-resolved -f
-		
-		echo -e "[*] Applying DNSCrypt-Proxy configurations ..."
+
+		echo -e "================================================="
+		echo -e "|   Applying DNSCrypt-Proxy configurations ...  |"
+		echo -e "================================================="
 		rm -rf /etc/dnscrypt-proxy/dnscrypt-proxy.toml
 		cp dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-		
-		echo -e "[*] Initializing  DNSCrypt-Proxy ...\n"
+
+		echo -e "========================================"
+		echo -e "|   Initializing  DNSCrypt-Proxy ...   |"
+		echo -e "========================================"
 		systemctl enable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
-		echo -e "\n[*] Checking DNSCrypt-Proxy Service Status . . .\n"
+		
+		echo -e "===================================================="
+		echo -e "|   Checking DNSCrypt-Proxy Service Status . . .   |"
+		echo -e "===================================================="
 		dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml -service restart
 		dnscrypt-proxy -config /etc/dnscrypt-proxy/dnscrypt-proxy.toml -check
-		
-		echo -e "\n[*] Applying IPTables/Firewall Ruleset . . ."
+
+		echo -e "================================================"
+		echo -e "|   Applying IPTables/Firewall Ruleset . . .   |"
+		echo -e "================================================"
 		iptables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
 		iptables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination 127.0.0.1:5354
 		# ip6tables -t nat -A OUTPUT -p udp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:5354
-		# ip6tables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:5354					
-		
-		echo -e "[*] Configuring & Restarting NetworkManager . . ."
+		# ip6tables -t nat -A OUTPUT -p tcp ! -d 91.239.100.100 --dport 53 -j DNAT --to-destination [::1]:5354
+
+		echo -e "===================================================="
+		echo -e "|   Configuring & Restarting NetworkManager . . .  |"
+		echo -e "===================================================="
 		rm -rf /etc/NetworkManager/NetworkManager.conf
 		rm -rf /etc/resolv.conf
-		echo -e "[device]\nwifi.scan-rand-mac-address=yes\nethernet.cloned-mac-address=random\nwifi.cloned-mac-address=stable\n\n[main]\ndns=none" > /etc/NetworkManager/NetworkManager.conf
+		echo -e "[device]\nwifi.scan-rand-mac-address=yes" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "ethernet.cloned-mac-address=random" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "wifi.cloned-mac-address=stable\n" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "[main]\ndns=none" >> /etc/NetworkManager/NetworkManager.conf
 		echo -e "nameserver ::1\nnameserver 127.0.0.1\noptions edns0 single-request-reopen" > /etc/resolv.conf
 		systemctl restart --now NetworkManager -f
 		
-		echo -e "\n[*] Anonymized DNSCrypt-Proxy-Linux Successfully Configured !
-		[*] Enjoy Anonymity & Kick The Asshole Snoopers !
-		[*] Rebooting Within 3s ...\n"
-		sleep 3 && reboot
+		echo -e "===================================="
+		echo -e "|  Reboot System To Take Effect !  |"
+		echo -e "===================================="
 	
 	elif [[ ${input} == 2 ]]
 	then
-		echo -e "[*] Hold Your Horses !\nReverting All Settings To Default State . . .\n"
-		echo -e "[*] Killing NetworkManager ..."
+		echo -e "==================================================="
+		echo -e "|  Reverting All Settings To Default State . . .  |"
+		echo -e "==================================================="
+		echo -e "|  Killing NetworkManager ...  |"
+		echo -e "================================"
 		systemctl stop --now NetworkManager -f
-		echo -e "[*] Disabling DNSCrypt-Proxy ..."
+
+		echo -e "=================================="
+		echo -e "|  Disabling DNSCrypt-Proxy ...  |"
+		echo -e "=================================="
 		systemctl disable --now dnscrypt-proxy.socket dnscrypt-proxy.service -f
 		
-		echo -e "\n[*] Uninstalling DNSCrypt-Proxy ...\n"
+		echo -e "====================================="
+		echo -e "|  Uninstalling DNSCrypt-Proxy ...  |"
+		echo -e "====================================="
 		if ! [ -z `which pacman 2 > /dev/null` ] && [ `nmcli networking` = "enabled" ]; # Arch
 		then
 			pacman -Rcnsu dnscrypt-proxy --noconfirm
@@ -98,21 +127,28 @@ else
 			emerge dnscrypt-proxy -Cv
 		fi
 
-		echo -e "[*] Reverting DNSCrypt-Proxy Configurations ..."
+		echo -e "================================================="
+		echo -e "|  Reverting DNSCrypt-Proxy Configurations ...  |"
+		echo -e "================================================="
 		rm -rf /etc/NetworkManager/NetworkManager.conf
 		rm -rf /etc/resolv.conf
 		rm -rf /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-		echo -e '[device]\nwifi.scan-rand-mac-address=yes\nethernet.cloned-mac-address=random\nwifi.cloned-mac-address=stable' > /etc/NetworkManager/NetworkManager.conf
+		echo -e "[device]\nwifi.scan-rand-mac-address=yes\n" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "ethernet.cloned-mac-address=random\n" >> /etc/NetworkManager/NetworkManager.conf
+		echo -e "wifi.cloned-mac-address=stable" >> /etc/NetworkManager/NetworkManager.conf
 		
-		echo -e "[*] Restarting SystemD-Resolved ..."
+		echo -e "====================================="
+		echo -e "|  Restarting SystemD-Resolved ...  |"
+		echo -e "====================================="
 		systemctl enable --now systemd-resolved -f
-		
-		echo -e "[*] Restarting NetworkManager ..."
+
+		echo -e "==================================="
+		echo -e "|  Restarting NetworkManager ...  |"
+		echo -e "==================================="
 		systemctl restart --now NetworkManager -f
 		
-		echo -e "\n[*] Anonymized DNSCrypt-Proxy-Linux Successfully Deconfigured
-		[*] Let Snoopers Show You Their Middle Fingers
-		[*] Rebooting Within 3s ...\n"
-                sleep 3 && reboot
+		echo -e "===================================="
+		echo -e "|  Reboot System To Take Effect !  |"
+		echo -e "===================================="
 	fi
 fi
